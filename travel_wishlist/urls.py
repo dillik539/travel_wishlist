d2c7814b48440d15593deafb36652a0ff81da152
clara <clara@minneapolis.edu>
2018-04-23 18:57:58 -0500

    fix for individual place pages

--- travel_wishlist/urls.py
+++ travel_wishlist/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     url(r'^$', views.place_list, name = 'place_list'),
     url(r'^visited$', views.places_visited, name = 'places_visited'), #add url for places visited
     # url(r'^isvisited$', views.place_is_visited, name = 'place_is_visited'), #add url for each visited places
-    url(r'^place/$', views.place_info, name = 'place_info'), # add a url for all the places information
+    url(r'^place/(?P<pk>\d+)/$', views.place_info, name = 'place_info'), # add a url for all the places information
 ]
